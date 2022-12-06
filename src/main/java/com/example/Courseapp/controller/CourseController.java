67247127175f52f0c60233f83838fb1a5bc9c2bd
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-06 11:05:33 +0530

    add

--- src/main/java/com/example/Courseapp/controller/CourseController.java
+++ src/main/java/com/example/Courseapp/controller/CourseController.java
@@ -1,7 +1,13 @@
 package com.example.Courseapp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 
 public class CourseController {
+    @PostMapping("/")
+    public String Addpage(){
+        return "Welcome to my add page";
+    }
 }
